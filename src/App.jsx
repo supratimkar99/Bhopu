534eb4de4223a4513ab0e9b8f4a738002f8f60e1
supratimkar99 <supratim.kar099@gmail.com>
2026-01-24 03:19:25 +0530

    Add copyright

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import memes from './memes';
 function App() {
 
   return (
-    <div className="gallery">
-      {Object.entries(memes).map(([key, meme]) => (
-        <SoundPlayer
-          key={key}
-          clickSound={meme.sound}
-          memeImage={meme.image}
-          altText={meme.altText}
-        />
-      ))}
-    </div>
+    <>
+      <div className="gallery">
+        {Object.entries(memes).map(([key, meme]) => (
+          <SoundPlayer
+            key={key}
+            clickSound={meme.sound}
+            memeImage={meme.image}
+            altText={meme.altText}
+          />
+        ))}
+      </div>
+      <footer className="footer">©Supratim</footer>
+    </>
   )
 }
 
